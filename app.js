414030cec855e46deb0a30564810f5fa39dba66f
Eduard2981 <104999916+Eduard2981@users.noreply.github.com>
2022-11-18 02:55:53 +0200

    Add style to all pages

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const ejs=require("ejs")
 const https=require("https")
 const date=require("./date.js")
 var _ = require("lodash");
+const { title } = require("process");
 const app=express()
 const port= 3000;
 app.set("view engine", "ejs")
@@ -16,14 +17,13 @@ app.use(bodyParser.urlencoded({extended: true}))
   let num3 = 0
 
 const articleData = {
-  title: "Your title",
+  title: "ERROR 404",
   imgUrl:
     "https://img.freepik.com/premium-vector/internet-connection-problem-concept-illustration-404-found-error-page-isolated-white-background-funny-gray-cat-isolated-vector-illustrations_450656-204.jpg?w=2000",
-  description:
-    '"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"',
+  description: "Whoops! This is not what you were looking for",
   paragraph:
-    '"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains.""',
-  blogDate: "12.34.5678, 12:34:56",
+    'In computer network communications, the HTTP 404, 404 not found, 404, 404 error, page not found or file not found error message is a hypertext transfer protocol (HTTP) standard response code, to indicate that the browser was able to communicate with a given server, but the server could not find what was requested. The error may also be used when a server does not wish to disclose whether it has the requested information.The website hosting server will typically generate a 404 Not Found web page when a user attempts to follow a broken or dead link; hence the 404 error is one of the most recognizable errors encountered on the World Wide Web.',
+  blogDate: "12.34.5678, 12:34:56"
 };
 
 // Num ---- articleData
@@ -31,13 +31,13 @@ const articleData = {
 // Num3 ---- location
 
 
-let num = 0;
+
 
 let weatherlocs = [{}];
 let posts = [articleData]; 
 let topics=['posts',undefined,'compose','contact']
 let num2 = 0
-
+let num = 0;
 
   app.get("/", function(req,res){
  res.redirect("/posts")
@@ -80,13 +80,15 @@ app.get("/contact", function(req,res){
     wheaterDesc: weatherlocs[num2].weatherDesc,
   });
 })
+
 app.post("/contact", function(req,res){
   res.redirect("/contact")
 })
 
   app.get("/posts", function (req, res) {
+
     res.render("posts", {
-      posts: posts,
+      
       title: posts[num].title,
       imgUrl: posts[num].imgUrl,
       description: posts[num].description,
@@ -98,6 +100,7 @@ app.post("/contact", function(req,res){
       wheaterImg: weatherlocs[num2].iconUrl,
       wheaterDesc: weatherlocs[num2].weatherDesc,
     });
+   
     console.log(num2);
     console.log(num3);
     console.log(location);
@@ -161,9 +164,12 @@ app.post("/posts", function(req, res){
 
 
 app.get("/posts/:postName", function (req, res) {
-const titleSent = _.kebabCase(req.params.postName); 
-const itemTitle = _.kebabCase(item.title)
-  posts.forEach(function(item){
+const titleSent = _.lowerCase(req.params.postName); 
+
+  
+posts.forEach(function(item){
+
+const itemTitle = _.lowerCase(item.title);
 
       if (titleSent === itemTitle) {
             res.render("home", {
@@ -179,9 +185,28 @@ const itemTitle = _.kebabCase(item.title)
               wheaterDesc: weatherlocs[num2].weatherDesc,
             });
         console.log("Match found!");
+        req.params.postName= _.kebabCase(req.params.postName)
+      }
+      else{
+        res.render("home", {
+          title: articleData.title,
+          imgUrl: articleData.imgUrl,
+          description: articleData.description,
+          paragraph: articleData.paragraph,
+          blogDate: articleData.blogDate,
+          cityName: weatherlocs[num2].locName,
+          temp: weatherlocs[num2].temp,
+          wheaterImg: weatherlocs[num2].iconUrl,
+          wheaterDesc: weatherlocs[num2].weatherDesc,
+        });
       }
   })
 });
+
+app.post("/posts/:postName", function (req,res){
+  const postName = req.params.postName
+
+})
 
 app.get("/failure", function(req,res){
   res.render("failure")
