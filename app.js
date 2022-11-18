673a32881756f58fbf566d44c098e5543c1990db
Eduard2981 <104999916+Eduard2981@users.noreply.github.com>
2022-11-18 21:02:31 +0200

    Update v3.0

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser=require("body-parser");
 const ejs=require("ejs")
 const https=require("https")
 const date=require("./date.js")
-var _ = require("lodash");
+const _ = require("lodash");
 const { title } = require("process");
 const app=express()
 const port= 3000;
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 
   // Open Weather API
   
-  let location = ['Liverpool']
+  const location = ['Liverpool']
   let num3 = 0
 
 const articleData = {
@@ -30,21 +30,30 @@ const articleData = {
 // Num2 ---- weatherlocs
 // Num3 ---- location
 
+const Liverpool=  {
+    title:
+      "Liverpool a câștigat Champions League, după 2-0 cu Tottenham. Klopp a spart gheața, după două finale pierdute.",
+    imgUrl:
+      "https://s.iw.ro/gateway/g/ZmlsZVNvdXJjZT1odHRwJTNBJTJGJTJG/c3RvcmFnZTAxdHJhbnNjb2Rlci5yY3Mt/cmRzLnJvJTJGc3RvcmFnZSUyRjIwMTkl/MkYwNiUyRjAyJTJGMTA4MDc3M18xMDgw/NzczX2p1cmdlbi1rbG9wcC5qcGcmdz0x/MDYwJmg9NjM2Jmhhc2g9OGUzMzk0YzQ3NTY4OTIzNTM1Y2Q1ZWEwNzc4M2U5ZTE=.thumb.jpg",
+    description:
+      "Liverpool a cucerit pentru a şasea oară trofeul UEFA Champions League, după ce a învins Tottenham în ultimul act al competiţiei. Duelul de pe Wanda Metropolitano a debutat cu cel mai rapid penalty din istoria finalelor competiţiei, dictat în secunda 23 şi confirmat de arbitrajul video. Egipteanul Salah l-a transformat, la prima atingere de balon.",
+    paragraph:
+      "După „afacerea englezească” din finala Europa League, câștigată de Chelsea, a venit rândul formației antrenate de Jurgen Klopp să câștige cel mai râvnit trofeu din fotbalul european. Finalele ambelor competiții, Europa League și UEFA Champions League, au fost disputate în 2019 de echipe englezești. În Liga Campionilor au mai fost până acum şase finale între echipe din aceeaşi ţară. Liverpool a fost la a doua finală consecutivă în Champions League după ce anul trecut a fost învinsă cu 3-1 de Real Madrid, în timp ce Tottenham a disputat prima sa finală în această competiţie. Liverpool a mai câştigat Liga Campionilor în 1977, 1978, 1981, 1984 şi 2005. Pe stadionul Metropolitano din Madrid, au marcat egipteanul Mohamed Salah în minutul 2, din penalty, şi belgianul Divock Origi, în minutul 87. Reuşita lui Salah ocupă locul doi în topul celor mai rapide deschideri de scor dintr-o finală de LC. Cel mai rapid gol a fost cel marcat de Paolo Maldini (AC Milan) în secunda 51 a finalei din 2005 cu Liverpool. Van Dijk de la Liverpool a fost desemnat jucătorul meciului. Meciul a fost precedat de un minut de reculegere în memoria fostului internaţional spaniol Jose Antonio Reyes, care a murit într-un accident rutier. De asemenea, în cadrul ceremoniei de deschidere, trupa americană Imagine Dragons a susţinut un recital. Finala a avut și un moment amuzant: a fost oprită în min. 18, când o tânără în costum de baie a intrat pe teren.",
+    blogDate: "Publicat pe 02.06.2019 la 11:35:24",
+  }
 
 
+const weatherlocs = [{}];
+const posts = [Liverpool]; 
 
-let weatherlocs = [{}];
-let posts = [articleData]; 
-let topics=['posts',undefined,'compose','contact']
 let num2 = 0
 let num = 0;
+
 
   app.get("/", function(req,res){
  res.redirect("/posts")
 
   });
-
-
 
 
 app.get("/compose", function(req, res){
@@ -68,7 +77,7 @@ const post={
 
  posts.push(post)
 num++
- res.redirect("/posts")
+ res.redirect("/compose")
 })
 
 
@@ -81,19 +90,10 @@ app.get("/contact", function(req,res){
   });
 })
 
-app.post("/contact", function(req,res){
-  res.redirect("/contact")
-})
-
   app.get("/posts", function (req, res) {
 
     res.render("posts", {
       
-      title: posts[num].title,
-      imgUrl: posts[num].imgUrl,
-      description: posts[num].description,
-      paragraph: posts[num].paragraph,
-      blogDate: posts[num].blogDate,
       posts: posts,
       cityName: weatherlocs[num2].locName,
       temp: weatherlocs[num2].temp,
@@ -101,10 +101,7 @@ app.post("/contact", function(req,res){
       wheaterDesc: weatherlocs[num2].weatherDesc,
     });
    
-    console.log(num2);
-    console.log(num3);
-    console.log(location);
-    console.log(weatherlocs);
+ 
   });
 
 app.post("/posts", function(req, res){
@@ -151,7 +148,8 @@ app.post("/posts", function(req, res){
         weatherlocs.push(weatherLoc)
         num2++
       });
-    res.redirect("/posts")
+        res.redirect("/posts");
+    
     }
     
   });
@@ -164,53 +162,72 @@ app.post("/posts", function(req, res){
 
 
 app.get("/posts/:postName", function (req, res) {
-const titleSent = _.lowerCase(req.params.postName); 
+const requestedTitle = _.lowerCase(req.params.postName); 
 
   
-posts.forEach(function(item){
+posts.forEach(function(post){
 
-const itemTitle = _.lowerCase(item.title);
+const storedTitle = _.lowerCase(post.title);
+  if (storedTitle===requestedTitle){
+ res.render("home", {
+   title: post.title,
+   imgUrl: post.imgUrl,
+   description: post.description,
+   paragraph: post.paragraph,
+   blogDate: post.blogDate,
 
-      if (titleSent === itemTitle) {
-            res.render("home", {
-              title: posts[num].title,
-              imgUrl: posts[num].imgUrl,
-              description: posts[num].description,
-              paragraph: posts[num].paragraph,
-              blogDate: posts[num].blogDate,
-              posts: posts,
-              cityName: weatherlocs[num2].locName,
-              temp: weatherlocs[num2].temp,
-              wheaterImg: weatherlocs[num2].iconUrl,
-              wheaterDesc: weatherlocs[num2].weatherDesc,
-            });
-        console.log("Match found!");
-        req.params.postName= _.kebabCase(req.params.postName)
-      }
-      else{
-        res.render("home", {
-          title: articleData.title,
-          imgUrl: articleData.imgUrl,
-          description: articleData.description,
-          paragraph: articleData.paragraph,
-          blogDate: articleData.blogDate,
-          cityName: weatherlocs[num2].locName,
-          temp: weatherlocs[num2].temp,
-          wheaterImg: weatherlocs[num2].iconUrl,
-          wheaterDesc: weatherlocs[num2].weatherDesc,
-        });
-      }
+   cityName: weatherlocs[num2].locName,
+   temp: weatherlocs[num2].temp,
+   wheaterImg: weatherlocs[num2].iconUrl,
+   wheaterDesc: weatherlocs[num2].weatherDesc,
+ });
+ console.log("Match found!");
+
+}
+
   })
 });
 
-app.post("/posts/:postName", function (req,res){
-  const postName = req.params.postName
-
-})
 
 app.get("/failure", function(req,res){
-  res.render("failure")
+     res.render("home", {
+       title: articleData.title,
+       imgUrl: articleData.imgUrl,
+       description: articleData.description,
+       paragraph: articleData.paragraph,
+       blogDate: articleData.blogDate,
+       cityName: weatherlocs[num2].locName,
+       temp: weatherlocs[num2].temp,
+       wheaterImg: weatherlocs[num2].iconUrl,
+       wheaterDesc: weatherlocs[num2].weatherDesc,
+     });
 })
+
+app.get("/control", function(req,res){
+  res.render("control", {
+    cityName: weatherlocs[num2].locName,
+    temp: weatherlocs[num2].temp,
+    wheaterImg: weatherlocs[num2].iconUrl,
+    wheaterDesc: weatherlocs[num2].weatherDesc,
+  });
+})
+
+
+app.get("*", function (req, res) {
+  res.status(404);
+  res.render("home", {
+    title: articleData.title,
+    imgUrl: articleData.imgUrl,
+    description: articleData.description,
+    paragraph: articleData.paragraph,
+    blogDate: articleData.blogDate,
+    cityName: weatherlocs[num2].locName,
+    temp: weatherlocs[num2].temp,
+    wheaterImg: weatherlocs[num2].iconUrl,
+    wheaterDesc: weatherlocs[num2].weatherDesc,
+  });
+});
+
 
 app.listen(port, function(){
     console.log("Server started on port "+port+".")
